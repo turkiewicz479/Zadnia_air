22c5872324d967293d9657609096e8ee5738e356
Szymon Turkiewicz <turkiewicz@student.agh.edu.pl>
2023-05-27 03:50:05 +0200

    zmianny w catalogue-1

--- skeletons/catalogue-1/catalogue.py
+++ skeletons/catalogue-1/catalogue.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import from typing import  Mapping
+from copy import copy, deepcopy
+from typing import Callable, Mapping, Optional
+
 
 class Product:
     def __init__(self, id_: str, name: str, price: float) -> None:
@@ -8,18 +10,38 @@ class Product:
         self.id = id_
         self.name = name
         self.price = price
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price: float) -> None:
+        self.__price = min([price, 100])
+
+
     def __str__(self)->str:
         return '{self.name} [{self.id}] : ${self.price:.2f}'.format(self=self)
     def __eq__(self, other)-> bool:
         return self.id == other.id and self.name == other.name and self.price == other.price
+
+
     pass
+
 
     # TODO: Zaimplementuj.
 
 
 class Catalogue:
     Inventory = Mapping[str, Product]
-    def __init__(self, inventory:Inventory):
+    def __init__(self, inventory:Inventory=None)->None:
+        self.inventory = deepcopy(inventory) if inventory else{}
+
+    def add_product(self, product: Product)->None:
+        self.inventory[product.id]= copy(product)
+    def __contains__(self, id_: str)-> bool:
+        return id_ in self.inventory
+
 
     # TODO: Zaimplementuj.
     pass
