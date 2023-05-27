65d280d8ad75bd6fa0585819beef6b8a66992109
Szymon Turkiewicz <turkiewicz@student.agh.edu.pl>
2023-05-27 23:58:00 +0200

    zmianny w catalogue-1

--- skeletons/catalogue-1/catalogue.py
+++ skeletons/catalogue-1/catalogue.py
@@ -7,7 +7,7 @@ from typing import Callable, Mapping, Optional
 class Product:
     def __init__(self, id_: str, name: str, price: float) -> None:
         # TODO: Zaimplementuj.
-        self.id = id_
+        self.id = id_ if id_ is not None else self.generate_id(name)
         self.name = name
         self.price = price
 
@@ -15,15 +15,19 @@ class Product:
     def price(self) -> float:
         return self.__price
 
-    @ste_price
-    def price(self, price: float) -> None:
+    @price.setter
+    def price(self) -> None:
         self.__price = min([price, 100])
 
-
-    def __str__(self)->str:
+    def __str__(self) -> str:
         return '{self.name} [{self.id}] : ${self.price:.2f}'.format(self=self)
-    def __eq__(self, other)-> bool:
+
+    def __eq__(self, other) -> bool:
         return self.id == other.id and self.name == other.name and self.price == other.price
+
+    @classmethod
+    def generate_id(cls, name: str) -> str:
+        return ''.join([c for c in name if c != ' ']) + '_' + str(len(name))
 
 
     pass
