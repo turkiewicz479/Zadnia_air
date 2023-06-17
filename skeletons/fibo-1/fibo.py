6a248c45c38ddd9899668dbf12edd30fb5af5e4d
Szymon Turkiewicz <turkiewicz@student.agh.edu.pl>
2023-06-17 06:59:34 +0200

    Dodanie Pi do consts, utworzenie zawartosci fibo i odpowiednich importów

--- skeletons/fibo-1/fibo.py
+++ skeletons/fibo-1/fibo.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from mymath.consts import *
+from mymath.sequences import recursive
+from mymath.sequences.recursive import fib
+
+
+print(recursive.fib(3))
+print(fib(3))
+print(PI)
--- skeletons/fibo-1/mymath/consts.py
+++ skeletons/fibo-1/mymath/consts.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+PI = 3.141592
